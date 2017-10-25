9671ccc526ccf1118a0a431152170106841d0581
Ellen Tang <ellen.tang@flex.com>
2017-10-25 00:46:56 -0700

    [Studio] Add screen "BLANK"

--- client/states.js
+++ client/states.js
@@ -42,5 +42,14 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
+    }).state('app.blank', {
+        views: {
+            app: {
+                controller: 'app_blank',
+                templateProvider: function (app) {
+                    return app.templateProvider('app.blank');
+                }
+            }
+        }
     });
 });
