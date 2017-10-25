6c49e191a151323bc4321aebc896b9f545f00e51
Ellen Tang <ellen.tang@flex.com>
2017-10-25 00:24:42 -0700

    [Studio] Modified screen "viewissues"

--- client/states.js
+++ client/states.js
@@ -6,12 +6,12 @@ angular.module('app').config(function ($stateProvider) {
         templateProvider: function (app) {
             return app.templateProvider('app');
         }
-    }).state('app.blank', {
+    }).state('app.viewissues', {
         views: {
             app: {
-                controller: 'app_blank',
+                controller: 'app_viewissues',
                 templateProvider: function (app) {
-                    return app.templateProvider('app.blank');
+                    return app.templateProvider('app.viewissues');
                 }
             }
         }
